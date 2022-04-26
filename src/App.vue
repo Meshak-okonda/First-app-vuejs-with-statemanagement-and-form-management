7dbea9e9a5fe6c355f21a15a4160a8d8c4ed2e04
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-04-26 12:37:03 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <FormUser />
+  <TableUser />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import FormUser from './components/FormUser.vue'
+import TableUser from './components/TableUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    FormUser,
+    TableUser
   }
 }
 </script>
